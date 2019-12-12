48e5a6279a3d7bd14c96f11d54878bbf747c835c
AbdeenM <abdeen.mohamed@outlook.com>
2019-12-12 12:59:50 +0200

    Testing VSCode Windows

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
  * Copyright(C) Mega Trade Website, Inc - All Rights Reserved
  * Unauthorized copying of this file, via any medium is strictly prohibited
  * Proprietary and confidential
- * Written by Abdeen Mohamed < abdeen.mohamed@outlook.com>, September 2019
+ *  by Abdeen Mohamed < abdeen.mohamed@outlook.com>, September 2019
  ************************************************************************** */
 
 import fs from 'fs'
