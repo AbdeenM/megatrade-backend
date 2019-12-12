0ea1032d20a32eb2aefcc7f593da54954086aee8
AbdeenM <abdeen.mohamed@outlook.com>
2019-12-12 14:07:09 +0200

    Testing for Heroku

--- build/index.js
+++ build/index.js
@@ -1,16 +1,12 @@
 'use strict';
 
-var _fs = require('fs');
-
-var _fs2 = _interopRequireDefault(_fs);
-
 var _cors = require('cors');
 
 var _cors2 = _interopRequireDefault(_cors);
 
-var _https = require('https');
+var _http = require('http');
 
-var _https2 = _interopRequireDefault(_https);
+var _http2 = _interopRequireDefault(_http);
 
 var _morgan = require('morgan');
 
@@ -38,17 +34,16 @@ var _Routes4 = _interopRequireDefault(_Routes3);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-const app = (0, _express2.default)(); /* **************************************************************************
-                                       * Copyright(C) Mega Trade Website, Inc - All Rights Reserved
-                                       * Unauthorized copying of this file, via any medium is strictly prohibited
-                                       * Proprietary and confidential
-                                       * Written by Abdeen Mohamed < abdeen.mohamed@outlook.com>, September 2019
-                                       ************************************************************************** */
+/* **************************************************************************
+ * Copyright(C) Mega Trade Website, Inc - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Abdeen Mohamed < abdeen.mohamed@outlook.com>, September 2019
+ ************************************************************************** */
 
-const server = _https2.default.createServer({
-	key: _fs2.default.readFileSync('server.key'),
-	cert: _fs2.default.readFileSync('server.cert')
-}, app);
+const app = (0, _express2.default)();
+const server = _http2.default.createServer(app);
+const PORT = process.env.PORT || 8080;
 
 _mongoose2.default.Promise = global.Promise;
 _mongoose2.default.connect('mongodb://localhost/megatrade', {
@@ -69,6 +64,6 @@ if (process.env.NODE_ENV !== 'production') app.use((0, _morgan2.default)('dev'))
 app.use('/api', [_Routes4.default, _Routes2.default]);
 app.use('/uploads', _express2.default.static(process.cwd() + '/uploads'));
 
-server.listen(8080, error => {
-	if (error) console.log(error);else console.log('Megatrade server started at port: 8080');
+server.listen(PORT, error => {
+	if (error) console.log(error);else console.log(`Megatrade server started at port: ${PORT}`);
 });
